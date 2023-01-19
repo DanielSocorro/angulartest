4453d2f8471ee6a85c54a4e20e7816b34b0e1a27
Daniel Socorro <daniel.socorro.c@gmail.com>
2023-01-19 16:55:53 -0300

    connecting with api

--- src/app/pokemons/pokemons.component.ts
+++ src/app/pokemons/pokemons.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
 
 @Component({
   selector: 'app-pokemons',
   templateUrl: './pokemons.component.html',
   styleUrls: ['./pokemons.component.css']
 })
-export class PokemonsComponent {
+export class PokemonsComponent implements OnInit{
 
+  name!: string;
+
+
+  constructor(private pokemonService : PokemonService) {}
+
+  ngOnInit(): void {
+  }
+
+  search() {
+    this.pokemonService.getPokemon(this.name).subscribe(data => {
+      console.log(data)
+    })
+  }
 }
